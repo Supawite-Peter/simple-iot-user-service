16a75e493080e4aed7acf09dbc612dc3d8dc4e1c
Supawite Kiatrattanakorn <supawite08@gmail.com>
2024-11-30 13:48:12 +0700

    Update auth service mock

--- src/users/mocks/auth.client.mock.ts
+++ src/users/mocks/auth.client.mock.ts
@@ -1,15 +1,38 @@
 import { of } from 'rxjs';
 
 export class AuthClientMock {
-  token: string;
+  accessTokenStub: string;
+  refreshTokenStub: string;
+  error: any;
 
-  constructor(token?: string) {
-    this.token = token;
+  constructor(accessTokenStub?: string, refreshTokenStub?: string) {
+    this.accessTokenStub = accessTokenStub;
+    this.refreshTokenStub = refreshTokenStub;
   }
 
   build() {
     return {
-      send: jest.fn().mockImplementation(() => of(this.token)),
+      send: jest
+        .fn()
+        .mockImplementation((pattern, payload) => this.send(pattern, payload)),
     };
+  }
+
+  send(pattern: any, payload: any) {
+    if (this.error) throw this.error;
+    if (pattern.cmd === 'auth.token.sign') {
+      switch (payload.type) {
+        case 'access':
+          return of({
+            user: { sub: payload.userId, username: payload.username },
+            token: this.accessTokenStub,
+          });
+        case 'refresh':
+          return of({
+            user: { sub: payload.userId, username: payload.username },
+            token: this.refreshTokenStub,
+          });
+      }
+    }
   }
 }
